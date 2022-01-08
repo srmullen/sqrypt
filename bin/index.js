c31e5f275ecb6fc9d41ec8a55d9a096293c27899
Sean Mullen <srmullen13@gmail.com>
2022-01-08 10:52:18 -0500

    initial commit

---
+++ bin/index.js
@@ -0,0 +1,3 @@
+#!/usr/bin/env node
+"use strict";
+console.log('sqrypt is tool for creating awesome npm/yarn scripts directly in your package.json');
---
+++ src/index.ts
@@ -0,0 +1,3 @@
+#!/usr/bin/env node
+
+console.log('sqrypt is tool for creating awesome npm/yarn scripts directly in your package.json');
